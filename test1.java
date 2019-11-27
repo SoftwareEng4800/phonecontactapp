76dffd6c7831223be572b9ec667ab625f299924f
YifeiShi99 <51186185+YifeiShi99@users.noreply.github.com>
2019-11-27 13:02:08 -0800

    "initial commit"

---
+++ test1.java
@@ -0,0 +1,26 @@
+package com.google.maps;
+
+import java.io.IOException;
+
+import java.util.Arrays; //
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+public class test1 {
+	public static void main(String[] args) throws ApiException, InterruptedException, IOException {
+		GeoApiContext context = new GeoApiContext.Builder()
+		    .apiKey("AIzaSyCj11Xr9nAVmCWloNL7O7Ea3FNhUanTfz8")  // key
+		    .build();
+		GeocodingResult[] results =  GeocodingApi.geocode(context,
+		    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();  // user address
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		//System.out.println(gson.toJson(results[0].addressComponents));  // prints out the address entered in JSON
+		
+		System.out.println(gson.toJson(results[0].geometry)); // reads result in JSON for the address just entered
+		
+		//System.out.println(Arrays.toString(results)); // prints out results as a whole
+	}
+}
